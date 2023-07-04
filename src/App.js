43f4a8dc4e8606fe77a60797f642765c1d2f6ac3
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2023-07-04 09:17:13 +0200

    add domain search

--- src/App.js
+++ src/App.js
@@ -8,19 +8,43 @@ import MapPanel from "./components/Map/Map";
 
 function App() {
   const URL =
-    "https://geo.ipify.org/api/v2/country,city?apiKey=at_O5tbsVn0INcLgsUP70mPiaQaVh5CU"; //U
+    "https://geo.ipify.org/api/v2/country,city?apiKey=at_O5tbsVn0INcLgsUP70mPiaQaVh5CU";
 
-  const fetchData = (ip) => {
-    axios
-      .get(`${URL}&ipAddress=${ip}`)
-      .then((res) => {
-        setIp(res.data.ip);
-        setLocation(res.data.location);
-        setIsp(res.data.isp);
-      })
-      .catch(function (error) {
-        toast.error(error.message);
-      });
+  const isIPAddress = (input) => {
+    const ipAddressPattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+    return ipAddressPattern.test(input);
+  };
+
+  const hasDomainCharacter = (input) => {
+    return /[.\s]/.test(input);
+  };
+
+  const fetchData = (input) => {
+    if (isIPAddress(input)) {
+      axios
+        .get(`${URL}&ipAddress=${input}`)
+        .then((res) => {
+          setIp(res.data.ip);
+          setLocation(res.data.location);
+          setIsp(res.data.isp);
+        })
+        .catch(function (error) {
+          toast.error(error.message);
+        });
+    } else if (hasDomainCharacter(input)) {
+      axios
+        .get(`${URL}&domain=${input}`)
+        .then((res) => {
+          setIp(res.data.ip);
+          setLocation(res.data.location);
+          setIsp(res.data.isp);
+        })
+        .catch(function (error) {
+          toast.error(error.message);
+        });
+    } else {
+      toast.error("Please use only IP or domain.");
+    }
   };
 
   const fetchUserData = () => {
